6eabc513a564bb1a682be7df484e30b034b6788e
Nico Vinzens <vinzensnico@hotmail.com>
2017-03-29 09:39:31 +0200

    fix stuff from conflict

--- src/test/java/ch/hsr/adit/business/service/UserServiceTest.java
+++ src/test/java/ch/hsr/adit/business/service/UserServiceTest.java
@@ -35,12 +35,8 @@ public class UserServiceTest {
     UserService userService = new UserService(userDao, roleService);
     User transientUser = new User();
     transientUser = userService.createUser(transientUser);
-    
-<<<<<<< Upstream, based on branch 'develop' of https://github.com/fabianhauser/engineering-projekt-server.git
     assertEquals(1L, transientUser.getId());
-=======
-    //assertEquals(new Long(1), transientUser.getId());
->>>>>>> 818ebd5 KeyStore & Token refactoring done
+
   }
 
 }
