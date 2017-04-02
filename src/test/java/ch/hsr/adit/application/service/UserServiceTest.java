3256e279d752a9092954edaf13f72f2c9dbf3483
Michael Wieland <michi.wieland@hotmail.com>
2017-04-02 18:20:16 +0200

    UserService Tests added

--- src/test/java/ch/hsr/adit/application/service/UserServiceTest.java
+++ src/test/java/ch/hsr/adit/application/service/UserServiceTest.java
@@ -1,7 +1,9 @@
 package ch.hsr.adit.application.service;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
 import org.junit.Test;
@@ -9,10 +11,10 @@ import org.junit.runner.RunWith;
 import org.mockito.Mock;
 import org.mockito.junit.MockitoJUnitRunner;
 
-import ch.hsr.adit.application.service.RoleService;
-import ch.hsr.adit.application.service.UserService;
+import ch.hsr.adit.domain.model.Role;
 import ch.hsr.adit.domain.model.User;
 import ch.hsr.adit.domain.persistence.UserDao;
+import spark.Request;
 
 @RunWith(MockitoJUnitRunner.class)
 public class UserServiceTest {
@@ -23,6 +25,8 @@ public class UserServiceTest {
   @Mock
   private RoleService roleService;
 
+  @Mock
+  private Request request;
   
   @Test
   public void createUserTest() {
@@ -36,7 +40,59 @@ public class UserServiceTest {
     User transientUser = new User();
     transientUser = userService.createUser(transientUser);
     assertEquals(1L, transientUser.getId());
-
+  }
+  
+  @Test
+  public void deleteUserByIdTest() {
+    User persistentUSer = new User();
+    persistentUSer.setId(1L);
+    when(userDao.get(1L)).thenReturn(persistentUSer);
+    
+    UserService userService = new UserService(userDao, roleService);
+    boolean response = userService.deleteUser(1L);
+    
+    verify(userDao).delete(any(User.class));;
+    assertTrue(response);
   }
 
+  @Test
+  public void transformUserAllTest() {
+    // arrange
+    User dbUser = new User();
+    dbUser.setId(1L);
+    when(userDao.get(1L)).thenReturn(dbUser);
+    
+    when(request.params(":id")).thenReturn("1");
+    when(request.queryParams("username")).thenReturn("natuil");
+    when(request.queryParams("email")).thenReturn("student@hsr.ch");
+    when(request.queryParams("passwordHash")).thenReturn("hufd17zduh10xdf");
+    when(request.queryParams("isPrivate")).thenReturn("true");
+    when(request.queryParams("wantsNotification")).thenReturn("true");
+    when(request.queryParams("isActive")).thenReturn("true");
+    when(request.queryParams("jwtToken")).thenReturn("u842588u44328u5.jsad8232235325.28395789fdhdasd");
+    when(request.queryParams("updated")).thenReturn("Sun Apr 02 15:55:59 UTC 2017");
+    when(request.queryParams("role")).thenReturn("1");
+    
+    Role role = new Role();
+    role.setId(1L);
+    role.setName("Admin");
+    when(roleService.getRole(1L)).thenReturn(role);
+
+    // act
+    UserService userService = new UserService(userDao, roleService);
+    User user = userService.transformToUser(request);
+    
+    // assert
+    assertEquals(1L, user.getId());
+    assertEquals("natuil", user.getUsername());
+    assertEquals("student@hsr.ch", user.getEmail());
+    assertEquals("hufd17zduh10xdf", user.getPasswordHash());
+    assertEquals(true, user.isIsPrivate());
+    assertEquals(true, user.isWantsNotification());
+    assertEquals(true, user.isIsActive());
+    assertEquals("u842588u44328u5.jsad8232235325.28395789fdhdasd", user.getJwtToken());
+    // TODO assertEquals(new Date(), user.getUpdated());
+    assertEquals(role, user.getRole());
+  }
+  
 }
