99c038f90e5d072a2a610a2749d0e3d2e6195b43
Michael Wieland <michi.wieland@hotmail.com>
2017-04-06 10:52:45 +0200

    Findbugs bugfix

--- src/main/java/ch/hsr/adit/util/DateUtil.java
+++ src/main/java/ch/hsr/adit/util/DateUtil.java
@@ -1,25 +1,23 @@
 package ch.hsr.adit.util;
 
-import java.text.DateFormat;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
 import org.apache.log4j.Logger;
 
-
 public class DateUtil {
 
   private static final Logger LOGGER = Logger.getLogger(DateUtil.class);
-  private static final DateFormat DATE_FORMAT = new SimpleDateFormat("dd.MM.yyyy");
+  private static final String DATE_FORMAT = "dd.MM.yyyy";
 
   public static String formatDate(Date date) {
-    return DATE_FORMAT.format(date);
+    return new SimpleDateFormat(DATE_FORMAT).format(date);
   }
 
   public static Date parseDate(String dateString) {
     try {
-      return DATE_FORMAT.parse(dateString);
+      return new SimpleDateFormat(DATE_FORMAT).parse(dateString);
     } catch (ParseException e) {
       LOGGER.warn("Unable to parse date " + dateString);
       return null;
