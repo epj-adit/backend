af683414b6c1c0d44f8f722ede51691ae493e5be
Michael Wieland <michi.wieland@hotmail.com>
2017-04-07 12:05:14 +0200

    unnecessary stubings removed

--- src/test/java/ch/hsr/adit/application/service/UserServiceTest.java
+++ src/test/java/ch/hsr/adit/application/service/UserServiceTest.java
@@ -21,36 +21,36 @@ public class UserServiceTest {
 
   @Mock
   private UserDao userDao;
-  
+
   @Mock
   private RoleService roleService;
 
   @Mock
   private Request request;
-  
+
   @Test
   public void createUserTest() {
     User newUser = new User();
     newUser.setId(1L);
     newUser.setEmail("student@hsr.ch");
-    
+
     when(userDao.persist(any(User.class))).thenReturn(newUser);
-    
+
     UserService userService = new UserService(userDao, roleService);
     User transientUser = new User();
     transientUser = userService.createUser(transientUser);
     assertEquals(1L, transientUser.getId());
   }
-  
+
   @Test
   public void deleteUserByIdTest() {
     User persistentUSer = new User();
     persistentUSer.setId(1L);
     when(userDao.get(1L)).thenReturn(persistentUSer);
-    
+
     UserService userService = new UserService(userDao, roleService);
     boolean response = userService.deleteUser(1L);
-    
+
     verify(userDao).delete(any(User.class));;
     assertTrue(response);
   }
@@ -61,7 +61,7 @@ public class UserServiceTest {
     User dbUser = new User();
     dbUser.setId(1L);
     when(userDao.get(1L)).thenReturn(dbUser);
-    
+
     when(request.params(":id")).thenReturn("1");
     when(request.queryParams("username")).thenReturn("natuil");
     when(request.queryParams("email")).thenReturn("student@hsr.ch");
@@ -69,10 +69,10 @@ public class UserServiceTest {
     when(request.queryParams("isPrivate")).thenReturn("true");
     when(request.queryParams("wantsNotification")).thenReturn("true");
     when(request.queryParams("isActive")).thenReturn("true");
-    when(request.queryParams("jwtToken")).thenReturn("u842588u44328u5.jsad8232235325.28395789fdhdasd");
-    when(request.queryParams("updated")).thenReturn("Sun Apr 02 15:55:59 UTC 2017");
+    when(request.queryParams("jwtToken"))
+        .thenReturn("u842588u44328u5.jsad8232235325.28395789fdhdasd");
     when(request.queryParams("role")).thenReturn("1");
-    
+
     Role role = new Role();
     role.setId(1L);
     role.setName("Admin");
@@ -81,7 +81,7 @@ public class UserServiceTest {
     // act
     UserService userService = new UserService(userDao, roleService);
     User user = userService.transformToUser(request);
-    
+
     // assert
     assertEquals(1L, user.getId());
     assertEquals("natuil", user.getUsername());
@@ -91,8 +91,7 @@ public class UserServiceTest {
     assertEquals(true, user.isWantsNotification());
     assertEquals(true, user.isIsActive());
     assertEquals("u842588u44328u5.jsad8232235325.28395789fdhdasd", user.getJwtToken());
-    // TODO assertEquals(new Date(), user.getUpdated());
     assertEquals(role, user.getRole());
   }
-  
+
 }
